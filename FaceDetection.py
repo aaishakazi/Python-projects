25c087da72bcc9dbf6a820288c19e7e2ceef44b0
aaishakazi <aaishakazi0808@gmail.com>
2026-03-29 12:03:04 +0300

    Uploading

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,11 +1,16 @@
 print("Face Detection using OpenCV")
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img = cv2.imread('test.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-cv2.imshow('img', img)
-cv2.waitKey()  
+def detect_faces(img_path):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('img', img)
+    cv2.waitKey()  
+    cv2.destroyAllWindows()
+
+img_path = input("Enter the image file path: ")
+detect_faces(img_path)
